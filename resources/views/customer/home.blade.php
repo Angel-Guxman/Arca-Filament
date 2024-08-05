a79fe236c2bc3bedb8d35a52164024ad36c199e1
Misael <misaelrosado254@gmail.com>
2024-08-05 02:03:18 -0500

    Cambio de rama y pequeñas correcciones

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.customer')
 @section('content')
-<style>
-  /*   .custom-size {
-        width: 90%;
-        height: 80%;
-    } */
-</style>
+    <style>
+        .custom-size {
+            width: 90%;
+            height: 80%;
+        }
+    </style>
     <section class=" p-4">
         <div class=" mx-10 my-5    md:h-[600px]  grid grid-cols-1 md:grid-cols-2 ">
             <section class=" md:grid hidden text-white  grid-cols-1 items-center justify-items-center ">
@@ -18,8 +18,8 @@
                 </div>
             </section>
             <div>
-                <img class=" py-10 px-10 border   object-contain brightness-90 h-full w-full   " src="{{ asset('images/rename.png') }}"
-                alt=""/>
+                <img class=" py-10 px-10 border   object-contain brightness-90 custom-size "
+                    src="{{ asset('images/rename.png') }}" alt="" />
             </div>
         </div>
     </section>
