34065160b3c5f44f0ddca78b7162fafdd2bf3e4a
Misael <misaelrosado254@gmail.com>
2024-08-07 06:18:11 -0500

    Cambios realizados

--- resources/views/customer/catalogue.blade.php
+++ resources/views/customer/catalogue.blade.php
@@ -12,7 +12,7 @@
 
         .carousel-container {
             display: flex;
-            width: calc(100% - 80px);
+            width: calc(100% - 70px);
             transition: transform 0.5s ease;
         }
 
@@ -42,6 +42,12 @@
             display: flex;
             justify-content: center;
             align-items: center;
+            opacity: 0;
+            transition: opacity 0.3s ease;
+        }
+
+        .carousel-wrapper:hover .carousel-button {
+            opacity: 1;
         }
 
         #prev {
@@ -58,11 +64,22 @@
             width: 100%;
             height: 800px;
         }
+        .filter {
+            border: 1px solid white;
+            width: 200px;
+            height: 28px;
+            transition: border-color 0.3s ease;
+        }
+
+        .filter:hover {
+        border-color: #A7F3D0; 
+    }
+
     </style>
 
     <h1 class="text-left text-white text-5xl ml-36 mb-12 mt-10">Catálogo</h1>
 
-    <div class="text-white flex ml-36">
+    <button class="text-white flex ml-36 hover:text-emerald-200 cursor-pointer filter">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round"
@@ -73,7 +90,7 @@
             class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
         </svg>
-    </div>
+    </button>
 
     <p class="text-right text-white -mt-6 mr-36"> 4 Catálogos</p>
 
@@ -81,11 +98,11 @@
         <button id='prev' class='carousel-button'><</button>
                 <div class="carousel-container">
                     @for ($i = 0; $i < 5; $i++)
-                        <div class=" carousel-item ">
+                        <a class=" carousel-item " href=''>
                             <img class="w-100 h-80 mb-2" src={{ asset('images/Pulsera.png') }} alt="Pulseras">
                             <p class="text-white">Pulsera Jade</p>
                             <p class="text-white ml-14">ir a productos</p>
-                        </div>
+                        </a>
                     @endfor
                 </div>
                 <button id='next' class="carousel-button">></button>
@@ -96,11 +113,11 @@
 
                 <div class="carousel-container">
                     @for ($i = 0; $i < 5; $i++)
-                        <div class=" carousel-item ">
+                        <a class=" carousel-item " href=''>
                             <img class="w-100 h-80 mb-2" src={{ asset('images/Collares.png') }} alt="Pulseras">
                             <p class="text-white">Collares Jade</p>
                             <p class="text-white ml-14">ir a productos</p>
-                        </div>
+                        </a>
                     @endfor
                 </div>
                 <button id='next' class="carousel-button">></button>
@@ -116,11 +133,11 @@
 
                 <div class="carousel-container">
                     @for ($i = 0; $i < 5; $i++)
-                        <div class=" carousel-item ">
+                        <a class=" carousel-item " href=''>
                             <img class="w-100 h-80 mb-2" src={{ asset('images/Aretes.png') }} alt="Pulseras">
                             <p class="text-white">Aretes Jade</p>
                             <p class="text-white ml-14">ir a productos</p>
-                        </div>
+                        </a>
                     @endfor
 
                 </div>
@@ -132,11 +149,11 @@
 
                 <div class="carousel-container">
                     @for ($i = 0; $i < 5; $i++)
-                        <div class=" carousel-item ">
+                        <a class=" carousel-item " href=''>
                             <img class="w-100 h-80 mb-2" src={{ asset('images/Anillos.png') }} alt="Pulseras">
                             <p class="text-white">Anillos Jade</p>
                             <p class="text-white ml-14">ir a productos</p>
-                        </div>
+                        </a>
                     @endfor
                 </div>
                 <button id='next' class="carousel-button">></button>
@@ -155,10 +172,18 @@
                 const itemsPerPage = 4;
                 const totalItems = carousel.querySelectorAll('.carousel-item').length;
 
-                if (totalItems <= itemsPerPage) {
-                    nextButton.style.display = 'none';
-                    prevButton.style.display = 'none';
-                }
+                const updateButtonVisibility = () => {
+                    if (totalItems <= itemsPerPage) {
+                        nextButton.style.display = 'none';
+                        prevButton.style.display = 'none';
+                    }else {
+                        nextButton.style.display = 'flex';
+                        prevButton.style.display = 'flex';
+                    }
+                };
+
+                updateButtonVisibility();
+
 
                 nextButton.addEventListener('click', () => {
                     if (currentIndex < totalItems - itemsPerPage) {
